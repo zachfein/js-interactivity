d62c74dee50c4d890ca11b611d5847aa5b981c49
zachfein <zvf0307@gmail.com>
2022-04-06 15:51:55 -0600

    intermediate intstructions completed

--- index.js
+++ index.js
@@ -2,23 +2,28 @@ let message = document.querySelector('#message')
 
 const addMovie = (event) => {
     event.preventDefault()
+    
     let inputField = document.querySelector('input')
     let movie = document.createElement('li')
     let movieTitle = document.createElement('span')
+    
     movieTitle.textContent = inputField.value
     movieTitle.addEventListener('click', crossOffMovie)
     movie.appendChild(movieTitle)
+    
     let deleteBtn = document.createElement('button')
     deleteBtn.textContent = 'X'
     deleteBtn.addEventListener('click', deleteMovie)
     movie.appendChild(deleteBtn)
+    
     document.querySelector('ul').appendChild(movie)
     inputField.value = ''
 }
 
 const deleteMovie = (event) => {
     event.target.parentNode.remove()
-    message.textContent = 'Movie deleted!'
+    message.textContent = event.target.parentNode.firstChild.textContent + ' deleted!'
+    revealMessage()
 }
 
 document.querySelector('form').addEventListener('submit', addMovie)
@@ -26,9 +31,18 @@ document.querySelector('form').addEventListener('submit', addMovie)
 const crossOffMovie = (event) => {
     event.target.classList.toggle('checked')
     if(event.target.classList.contains('checked')){
-        message.textContent = 'Movie watched!'
+        message.textContent = event.target.textContent + ' watched!'
     } else {
-        message.textContent = 'Movie added back!'
+        message.textContent = event.target.textContent + ' added back!'
     }
+    revealMessage()
+}
+
+const revealMessage = () => {
+        message.classList.remove('hide')
+    setTimeout(() => {
+        message.classList.add('hide')
+    }, 1000)
+
 }
 
